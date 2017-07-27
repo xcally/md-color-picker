354b77fb23f34c47c8d38727f1b1150ab93ada42
Nikolay Baluk <kolyaflash@gmail.com>
2017-07-27 17:09:24 +0300

    update again

--- dist/mdColorPicker.min.js
+++ dist/mdColorPicker.min.js
@@ -1,6 +1,6 @@
 /**
  * md-color-picker - Angular-Material inspired color picker.
- * @version v0.2.6
+ * @version v0.2.7
  * @link https://github.com/brianpkelley/md-color-picker
  * @license MIT
  */
